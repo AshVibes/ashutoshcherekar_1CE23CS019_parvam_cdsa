c664168741e963e39ba8a59962f540d3fda58727
Ashutosh Cherekar <iashutoshcherekar@gmail.com>
2024-11-08 14:29:03 +0530

    example

---
+++ nov8/example.c
@@ -0,0 +1,14 @@
+#include<string.h>
+int main()
+{
+	char a;
+	char b[20];
+	printf("Enter a character:");
+	scanf("%c",&a);
+	printf("Enter a string:"):
+	scanf("%s",b);
+	printf("character:%c",a);
+	printf("string:%s",b);
+	return 0;
+}
+
