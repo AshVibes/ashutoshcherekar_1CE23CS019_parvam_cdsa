6679eca4793eb6cc9fed419179c0d336854221d2
Ashutosh Cherekar <iashutoshcherekar@gmail.com>
2024-11-08 09:27:18 +0530

    Update listimp.c

--- Programs/listimp.c
+++ Programs/listimp.c
@@ -5,8 +5,6 @@ struct Node {
     int data;
     struct Node* next;
 };
-
-
 struct Node* createNode(int data) {
     struct Node* newNode = (struct Node*) malloc(sizeof(struct Node));
     if (newNode == NULL) {
@@ -17,32 +15,25 @@ struct Node* createNode(int data) {
     newNode->next = NULL;
     return newNode;
 }
-
 void insertEnd(struct Node** head, int data) {
     struct Node* newNode = createNode(data);
     if (*head == NULL) {
         *head = newNode;
         return;
     }
-
     struct Node* temp = *head;
     while (temp->next != NULL) {
         temp = temp->next;
     }
     temp->next = newNode;
 }
-
-
 void deleteNode(struct Node** head, int key) {
     if (*head == NULL) {
         printf("List is empty.\n");
         return;
     }
-
     struct Node* temp = *head;
     struct Node* prev = NULL;
-
-    
     if (temp != NULL && temp->data == key) {
         *head = temp->next;
         free(temp);
@@ -54,19 +45,14 @@ void deleteNode(struct Node** head, int key) {
         prev = temp;
         temp = temp->next;
     }
-
-
     if (temp == NULL) {
         printf("Node with value %d not found.\n", key);
         return;
     }
-
-   
     prev->next = temp->next;
     free(temp);
     printf("Node with value %d deleted.\n", key);
 }
-
 void search(struct Node* head, int key) {
     struct Node* temp = head;
     while (temp != NULL) {
@@ -91,30 +77,18 @@ void printList(struct Node* head) {
     }
     printf("NULL\n");
 }
-
 int main() {
     struct Node* head = NULL;  
-
-
     insertEnd(&head, 10);
     insertEnd(&head, 20);
     insertEnd(&head, 30);
     insertEnd(&head, 40);
     insertEnd(&head, 50);
-
-
     printList(head);
-
-
     search(head, 30);
     search(head, 60);
-
-
     deleteNode(&head, 20);
     deleteNode(&head, 60);
-
-    
     printList(head);
-
     return 0;
 }
